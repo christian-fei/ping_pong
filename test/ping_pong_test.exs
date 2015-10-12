47f64f0252e13b7ac315b3aa651f4d9c921f42e3
Christian Fei <crifei93@gmail.com>
2015-10-12 08:20:53 +0200

    updated tests

--- test/ping_pong_test.exs
+++ test/ping_pong_test.exs
@@ -2,13 +2,13 @@ defmodule PingPongTest do
   use ExUnit.Case
   doctest PingPong
 
-  test "it responds to ping with pong" do
+  test "responds to ping with pong" do
     pid = spawn_link PingPong, :start, []
     send pid, {:ping, self}
     assert_receive :pong
   end
 
-  test "it responds to multiple ping with pong" do
+  test "responds to multiple ping with pong" do
     pid = spawn_link PingPong, :start, []
     send pid, {:ping, self}
     assert_receive :pong
